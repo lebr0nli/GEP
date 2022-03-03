01f70049d3c19ea0242b14ff226018e641b0a986
lebr0nli <alan.li.tw@gmail.com>
2022-03-04 07:55:08 +0800

    no need to mkidr after pip

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 pip install --target="$HOME/GEP/" prompt_toolkit==2.0.10
-mkdir -p "$HOME/GEP/"
 cp gdbinit-gep "$HOME/GEP/.gdbinit-gep"
 cp gdbinit-gep.py "$HOME/GEP/.gdbinit-gep.py"
 cp geprc.py "$HOME/GEP/geprc.py"
