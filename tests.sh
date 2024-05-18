9090fd0f27c5f06adeec2e80973463feff3bc858
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-05-18 12:11:28 +0800

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-source ./.venv/bin/activate
+set -o errexit
+
+help_and_exit() {
+    echo "Usage: $0"
+    exit 1
+}
+
+if [[ $# -gt 0 ]]; then
+    help_and_exit
+fi
+
+set -o xtrace
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+GEP_BASE=$(pwd)
+# shellcheck disable=SC1091
+source "$GEP_BASE/.venv/bin/activate"
 pytest -v tests/
