b04c87f2521ff67f9faaf8f4b496fc25ec549362
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-05-18 11:14:26 +0800

    Fix lint

--- lint.sh
+++ lint.sh
@@ -29,7 +29,10 @@ done
 
 set -o xtrace
 
-source ./.venv/bin/activate
+cd "$(dirname "${BASH_SOURCE[0]}")"
+GEP_BASE=$(pwd)
+# shellcheck disable=SC1091
+source "$GEP_BASE/.venv/bin/activate"
 
 VERMIN_TARGETS=(
     "gdbinit-gep.py"
