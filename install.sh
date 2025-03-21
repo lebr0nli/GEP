4bfd34c0f33ad2b6b045e11fb74e37a6c525d788
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2025-03-22 00:14:04 +0800

    Update install.sh
    * `--skip-venv` to skip creating virtualenv
    * `--skip-gdbinit` to not setting up gdbinit

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ help_and_exit() {
     echo "Usage: $0 [-d|--dev]"
     cat << EOF
   -d,  --dev         install development dependencies
+  --skip-venv        skip creating virtualenv
+  --skip-gdbinit     do not set up gdbinit
 EOF
     exit 1
 }
 
-if [[ $# -gt 1 ]]; then
-    help_and_exit
-fi
-
 DEV=0
+SKIP_VENV=0
+SKIP_GDBINIT=0
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -22,7 +22,16 @@ while [[ $# -gt 0 ]]; do
             DEV=1
             shift
             ;;
+        --skip-venv)
+            SKIP_VENV=1
+            shift
+            ;;
+        --skip-gdbinit)
+            SKIP_GDBINIT=1
+            shift
+            ;;
         *)
+            echo "Unknown option: $1"
             help_and_exit
             ;;
     esac
@@ -33,35 +42,45 @@ GEP_BASE=$(pwd)
 GDBINIT_GEP_PY=$GEP_BASE/gdbinit-gep.py
 echo "GEP installation path: $GEP_BASE"
 
-# find python path
-PYVER=$(gdb -batch -q --nx -ex 'pi import platform; print(".".join(platform.python_version_tuple()[:2]))')
-PYTHON=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
-if ! uname -a | grep -q Darwin > /dev/null; then
-    PYTHON+="${PYVER}"
-fi
-
-# create venv and install prompt_toolkit
-VENV_PATH=$GEP_BASE/.venv
-echo "Creating virtualenv in path: ${VENV_PATH}"
-"$PYTHON" -m venv "$VENV_PATH"
-PYTHON=$VENV_PATH/bin/python
-echo "Installing dependencies"
-"$PYTHON" -m pip install -U pip
 if [[ $DEV == 1 ]]; then
     uv sync --group dev
 else
-    "$VENV_PATH/bin/pip" install --no-cache-dir -e .
+    if [[ $SKIP_VENV == 1 ]]; then
+        echo "Skipping virtualenv creation"
+    else
+        # find python path
+        PYVER=$(gdb -batch -q --nx -ex 'pi import platform; print(".".join(platform.python_version_tuple()[:2]))')
+        PYTHON=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
+        if ! uname -a | grep -q Darwin > /dev/null; then
+            PYTHON+="${PYVER}"
+        fi
+        # create venv and install prompt_toolkit
+        VENV_PATH=$GEP_BASE/.venv
+        echo "Creating virtualenv in path: ${VENV_PATH}"
+        "$PYTHON" -m venv "$VENV_PATH"
+        PYTHON=$VENV_PATH/bin/python
+        echo "Installing dependencies"
+        "$PYTHON" -m pip install -U pip
+        "$VENV_PATH/bin/pip" install --no-cache-dir -e .
+    fi
 fi
 
 # copy example config to GEP_BASE if not exists
 echo "Copying default config to $GEP_BASE if not exists"
-cp -n "$GEP_BASE"/example/* "$GEP_BASE"
+# Note: macOS will return 1 if the file already exists
+cp -n "$GEP_BASE"/example/* "$GEP_BASE" || true
 
 # append GEP to gdbinit if not exists
-if ! grep -q '^[^#]*source.*/gdbinit-gep.py' ~/.gdbinit; then
-    echo "Appending GEP to ~/.gdbinit"
-    printf '\n# Please make sure the following line is always the last in this file\n' >> ~/.gdbinit
-    printf 'source %s\n' "$GDBINIT_GEP_PY" >> ~/.gdbinit
+if [[ $SKIP_GDBINIT == 1 ]]; then
+    echo "Skipping gdbinit setup"
+else
+    if ! grep -q '^[^#]*source.*/gdbinit-gep.py' ~/.gdbinit; then
+        echo "Appending GEP to ~/.gdbinit"
+        printf '\n# Please make sure the following line is always the last in this file\n' >> ~/.gdbinit
+        printf 'source %s\n' "$GDBINIT_GEP_PY" >> ~/.gdbinit
+    fi
 fi
+
+echo "Installation complete!"
 
 exit 0
