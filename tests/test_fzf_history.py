7a8ec90675668c96863c7388a9570f21c6cb9b0a
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2026-01-27 03:19:06 +0800

    Split test_gep.py into separate test files

--- tests/test_fzf_history.py
+++ tests/test_fzf_history.py
@@ -4,45 +4,6 @@ from conftest import GDBSession
 FZF_POINTER = b"\xe2\x96\x8c"  # ▌
 
 
-def grey(b: bytes) -> bytes:
-    """
-    Return the grey version of the bytes.
-
-    :param bytes b: The bytes to make grey.
-    :return: The grey version of the bytes.
-    :rtype: bytes
-    """
-    return b"\x1b[38;5;241m" + b + b"\x1b[39m"
-
-
-def test_autosuggestion(gdb_session: GDBSession) -> None:
-    gdb_session.start(histories=["print 12", "print 34"])
-
-    # the autosuggestion should not be shown when no match
-    gdb_session.send_literal("print x")
-    assert b"(gdb) print x" == gdb_session.capture_pane(with_color=True)
-
-    # make buffer to "print "
-    gdb_session.send_key("BSpace")
-    gdb_session.send_key("BSpace")
-    gdb_session.send_literal(" ")
-    # match "print 34"
-    assert b"(gdb) print " + grey(b"34") == gdb_session.capture_pane(with_color=True)
-    # accept the suggestion
-    gdb_session.send_key("Right")
-    assert b"(gdb) print 34" == gdb_session.capture_pane(with_color=True)
-
-    # make buffer to "print 1"
-    gdb_session.send_key("BSpace")
-    gdb_session.send_key("BSpace")
-    gdb_session.send_literal("1")
-    # match "print 2"
-    assert b"(gdb) print 1" + grey(b"2") == gdb_session.capture_pane(with_color=True)
-    # accept the suggestion
-    gdb_session.send_key("Right")
-    assert b"(gdb) print 12" == gdb_session.capture_pane(with_color=True)
-
-
 def test_fzf_history_search(gdb_session: GDBSession) -> None:
     gdb_session.start(histories=["print 10", "print 11", "print 20"])
 
