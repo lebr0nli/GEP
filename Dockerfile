396dccf734821e37f3290c9e3617d75587b3c53f
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2026-01-27 14:36:38 +0800

    Add tests for fzf breakpoint toggle/delete feature

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
 ARG image
 FROM $image
 
-ENV DEBIAN_FRONTEND=noninteractive
-
 RUN apt-get update && \
-    apt-get install -y \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y \
     python3 \
     python3-pip \
     python3-venv \
     tmux \
     git \
     gdb \
+    gcc \
+    make \
     curl \
     wget \
     vim && \
--- tests.sh
+++ tests.sh
@@ -2,17 +2,11 @@
 
 set -o errexit
 
-help_and_exit() {
-    echo "Usage: $0"
-    exit 1
-}
-
-if [[ $# -gt 0 ]]; then
-    help_and_exit
-fi
-
 cd "$(dirname "${BASH_SOURCE[0]}")"
 GEP_BASE=$(pwd)
 # shellcheck disable=SC1091
 source "$GEP_BASE/.venv/bin/activate"
-pytest -v tests/
+
+make -C "$GEP_BASE/tests/fixtures" all
+
+pytest -v "$@"
